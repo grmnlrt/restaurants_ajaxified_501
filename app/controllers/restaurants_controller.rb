32a1da2209121384a218f1e1219d65baa48c0b0f
germain loret <germain.loret@gmail.com>
2020-11-20 10:30:55 +0100

    Ajaxified app

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,10 +1,20 @@
 class RestaurantsController < ApplicationController
   def index
     @restaurants = Restaurant.all
+    respond_to do |format|
+      format.html
+      format.json { render json: { restaurants: @restaurants } }
+    end
   end
 
   def show
     @restaurant = Restaurant.find(params[:id])
     @review = Review.new
+  end
+
+  def destroy
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.destroy
+    redirect_to restaurants_path
   end
 end
